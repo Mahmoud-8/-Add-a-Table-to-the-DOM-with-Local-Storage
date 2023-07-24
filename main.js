a776489d9cff42300c4509fc2b13049710a6803d
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-07-24 20:37:12 +0200

    done

--- main.js
+++ main.js
@@ -4,61 +4,74 @@ const inputFields = document.querySelectorAll(
   "#tableData1, #tableData2, #tableData3, #tableData4"
 );
 
-addTable.addEventListener("click", ahmed ) 
+let isEditing = false; 
+let editRow; 
+addTable.addEventListener("click", ahmed);
+
 function ahmed() {
   const myId = document.getElementById("tableData1").value;
   const myName = document.getElementById("tableData2").value;
   const myProfession = document.getElementById("tableData3").value;
   const myAddress = document.getElementById("tableData4").value;
 
-   const newRow = document.createElement("tr");
-  tableBody.appendChild(newRow);
+  if (isEditing && editRow) {
+   
+    editRow.cells[0].textContent = myId;
+    editRow.cells[1].textContent = myName;
+    editRow.cells[2].textContent = myProfession;
+    editRow.cells[3].textContent = myAddress;
 
-  const idCell = document.createElement("td");
-  idCell.textContent = myId;
-  newRow.appendChild(idCell);
+   
+    isEditing = false;
+    editRow = null;
+  } else {
+    
+    const newRow = document.createElement("tr");
+    tableBody.appendChild(newRow);
 
-  const nameCell = document.createElement("td");
-  nameCell.textContent = myName;
-  newRow.appendChild(nameCell);
+    const idCell = document.createElement("td");
+    idCell.textContent = myId;
+    newRow.appendChild(idCell);
 
-  const professionCell = document.createElement("td");
-  professionCell.textContent = myProfession;
-  newRow.appendChild(professionCell);
+    const nameCell = document.createElement("td");
+    nameCell.textContent = myName;
+    newRow.appendChild(nameCell);
 
-  const addressCell = document.createElement("td");
-  addressCell.textContent = myAddress;
-  newRow.appendChild(addressCell);
+    const professionCell = document.createElement("td");
+    professionCell.textContent = myProfession;
+    newRow.appendChild(professionCell);
 
-  ////////////
-  /////another way
-/* 
-  tableBody.innerHTML += ` 
-<tr> 
-<td> ${myId} </td>
- <td> ${myName}</td> 
- <td> ${myProfession}</td> 
-  <td>${myAddress}  </td>
-   </tr>
-`;
- */
-  const actionsCell = document.createElement("td");
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", function () {
-    newRow.remove();
-  });
+    const addressCell = document.createElement("td");
+    addressCell.textContent = myAddress;
+    newRow.appendChild(addressCell);
 
-  const updateButton = document.createElement("button");
-  updateButton.textContent = "Update";
-  updateButton.addEventListener("click", function () {
-    console.log("Update clicked for row:", newRow);
-  });
+    const actionsCell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function () {
+      newRow.remove();
+    });
 
-  actionsCell.appendChild(deleteButton);
-  actionsCell.appendChild(updateButton);
-  newRow.appendChild(actionsCell);
+    const updateButton = document.createElement("button");
+    updateButton.textContent = "Update";
+    updateButton.addEventListener("click", function () {
+      
+      isEditing = true;
+      editRow = newRow;
 
+     
+      document.getElementById("tableData1").value = editRow.cells[0].textContent;
+      document.getElementById("tableData2").value = editRow.cells[1].textContent;
+      document.getElementById("tableData3").value = editRow.cells[2].textContent;
+      document.getElementById("tableData4").value = editRow.cells[3].textContent;
+    });
+
+    actionsCell.appendChild(deleteButton);
+    actionsCell.appendChild(updateButton);
+    newRow.appendChild(actionsCell);
+  }
+
+  
   inputFields.forEach((input) => {
     input.value = "";
   });
